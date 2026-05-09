8a82c2d8bbd13c78968f7aed6e346edd383f4d1d
PepperySalt <145277049+PepperySalt@users.noreply.github.com>
2026-05-09 10:17:40 +0200

    correction of vocabulary

--- cisco_type_7_password_decrypter.py
+++ cisco_type_7_password_decrypter.py
@@ -1,37 +1,37 @@
-def reverse_hash(hashed_pw):
+def decrypter(encrypted_pass):
     xlat = list("dsfd;kfoA,.iyewrkldJKDHSUBsgvca69834ncxv9873254k;fg87")
-    hashed_pw = list(hashed_pw)
-    clear_pw = [-1] * (len(hashed_pw)//2 - 1)
+    encrypted_pass = list(encrypted_pass)
+    clear_pw = [-1] * (len(encrypted_pass)//2 - 1)
 
     val = 0
 
-    if(len(hashed_pw)%2 != 0):
+    if(len(encrypted_pass)%2 != 0):
         print("ERROR : incorrect encrypted password format due to odd lenght")
         return -1
 
 
-    seed = (int(hashed_pw[0])) * 10 + int(hashed_pw[1])
+    seed = (int(encrypted_pass[0])) * 10 + int(encrypted_pass[1])
 
-    if(seed > 15 or not(hashed_pw[0].isnumeric()) or not(hashed_pw[0].isnumeric()) ):
+    if(seed > 15 or not(encrypted_pass[0].isnumeric()) or not(encrypted_pass[0].isnumeric()) ):
         print("ERROR : incorrect encrypted password format due to the first two characters being wrong")
         return -1
 
 
-    for i in range(2,len(hashed_pw)+1,1):
+    for i in range(2,len(encrypted_pass)+1,1):
 
         if(i != 2 and i%2 == 0):
             clear_pw[i//2 - 2] = chr( val ^ ord(xlat[seed]) )
             seed += 1
             val = 0
 
-        if(i != len(hashed_pw)):
+        if(i != len(encrypted_pass)):
             val *=  16
 
-            if hashed_pw[i].isnumeric() : 
-                val += int(hashed_pw[i])
+            if encrypted_pass[i].isnumeric() : 
+                val += int(encrypted_pass[i])
             else:
-                hashed_pw[i] = hashed_pw[i].upper()
-                c = hashed_pw[i]
+                encrypted_pass[i] = encrypted_pass[i].upper()
+                c = encrypted_pass[i]
                 if ord(c) >= ord('A') and ord(c) <= ord('F') :
                     val += int(c, 16)
     
@@ -39,7 +39,7 @@ def reverse_hash(hashed_pw):
     return clear_pw
 
 
-#This is the cisco 7 hash of the string "password"
+#This is the cisco type 7 encription of the string "password"
 password = "021605481811003348"
-password = reverse_hash(password)
+password = decrypter(password)
 print(password)
